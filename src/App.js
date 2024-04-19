dbede54a7d01063310693f1943b7a98a49167994
codnjs042 <codnjs042@naver.com>
2024-04-19 18:44:02 +0900

    [Feat] view

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{backgroundColor:"LightGray", fontSize: "20px", display:"flex"}}>
+      <div style={{backgroundColor:"white", border:"5px solid", margin: "8% auto"}}>
+        <div style={{display:"flex", alignItems: "center"}}> 
+          <input style={{flex:1, margin:"50px", border:"3px solid", width:"100%", height:"50px", fontWeight: "bold", fontSize: "20px", textAlign: "center"}}></input>
+          <select style={{flex:1, margin:"50px", border:"3px solid", height:"50px", fontWeight: "bold", fontSize: "20px"}}>
+            <option >옵션</option>
+          </select>
+        </div>
+        <div style={{margin:"50px", marginTop:"0px", border:"3px solid"}}>
+          <div style={{ display: "flex", width: "100%" }}>
+            <div style={{ border:"1.5px solid", flex:1, textAlign: "center"}}>탭1</div>
+            <div style={{ border:"1.5px solid", flex:1, textAlign: "center" }}>탭2</div>
+          </div>
+          <div style={{borderTop: "0px", margin:"50px"}}>
+            <p style={{fontWeight: "bold", fontSize: "30px"}}>환율</p>기준일</div>
+          </div>
+      </div>
     </div>
   );
 }
